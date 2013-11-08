13a6c2e42ae60dfe99b539e4e98da2ea2eecc3e9
LunasAbacus <lunasabacus@gmail.com>
2013-11-08 12:08:06 -0500

    flushed out more of dbscan, couple more functions to complete

--- PythonCode/DBSCAN.py
+++ PythonCode/DBSCAN.py
@@ -10,11 +10,13 @@
 #!/usr/bin/env python
 
 import DistanceMetrics as Distance
+import ClusterPoint
 
 def init(self, MinGroupSize, DistanceThreshold, DistanceMetricType):
 	self.min = MinGroupSize
 	self.threshold = DistanceThreshold
 	self.data = []
+	self.highestCluster = 1
 	if (DistanceMetricType == 'Jaccard'):
 		self.useJaccard = True;
 	else:
@@ -32,21 +34,31 @@ def IsNoise(self, ArticleSet):
 	if (numClosePoints < self.min):
 		return True
 
-def ClosestPoint(self):
+def ClosestPoint(self, newSet):
 	pass
 
 def ClassifyCluster(self):
 	pass
 
 def AddArticle(self, ArticleName, ArticleTerms):
+	point = ClusterPoint(ArticleName, ArticleTerms)
+
 	#check if point is noise
-	if (not IsNoise(ArticleTerms)):
-		pass
+	if (not self.IsNoise(ArticleTerms)):
 		#if not, assign to closest cluster within threshold
+		closestPoint = self.ClosestPoint(ArticleTerms)
+		if (closestPoint is not None):
+			point.SetCluster(closestPoint.cluster)
 
 		#if no cluster exist, create new cluster and assign
+		else:
+			point.SetCluster(self.highestCluster)
+			self.highestCluster += 1
 
-def CleanUpOutliers():
+	self.data.append(point)
+
+
+def CleanUpOutliers(self):
 	#Mental note, handle outliers that aren't noise
 	#go through all the noise points cluster outliers
 	#if possible
@@ -55,7 +67,7 @@ def CleanUpOutliers():
 	pass
 
 def main():
-	print "This is an implementation of DBSCAN."
+	print ("This is an implementation of DBSCAN.")
 
 if __name__ == '__main__':
     main()
